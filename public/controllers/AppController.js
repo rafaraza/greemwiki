959f4f3b86e99f002dc7952f067aee3406d270b0
Mahelder <mahelder.melo@fcamara.com.br>
2015-08-07 16:27:26 -0300

    Login

--- public/controllers/AppController.js
+++ public/controllers/AppController.js
@@ -1,6 +1,6 @@
 var greemWiki = angular.module('GreemWiki', []);
 
-greemWiki.controller('AppController', ['$scope','$http', function($scope, $http){
+greemWiki.controller('AppController', ['$scope','$http','$window', function($scope, $http, $window){
 
 	var refresh = function(){
 			$http.get('/post').success(function(response){
@@ -8,5 +8,28 @@ greemWiki.controller('AppController', ['$scope','$http', function($scope, $http)
 		});
 	};
 
+	$scope.logar = function(user){
+		$http.post("/user/login", user).success(function(response){
+			//adicionando usuario na sessao
+			if(response.length > 0)
+				$window.sessionStorage.setItem('usuario', response[0].nome);
+		});
+	};
+
+	$scope.logado = function(){
+		var valor = $window.sessionStorage.getItem('usuario');
+		if(valor){
+			$scope.usuario = valor + "(Sair)";
+			return valor;
+		}
+		return null;
+	}
+
+	$scope.logout = function(){
+		$window.sessionStorage.removeItem('usuario');
+		$scope.usuario = "";
+	}
+
 	refresh();
+
 }]);
